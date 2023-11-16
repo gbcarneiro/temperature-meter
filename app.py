59067f2b391ea5ebfff6cd4b67d353fc5437f3f0
Gabriel Carneiro <gabriel.carneiro@sou.inteli.edu.br>
2023-11-16 00:50:59 -0300

    feat: add temperature

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, request 
+from flask import Flask, jsonify, request 
 from datetime import datetime
+from pymongo.mongo_client import MongoClient
 
 app = Flask(__name__)
+
+mongo_uri = f"mongodb+srv://gabrielcarneiro:{password}@cluster0.qfs6xtz.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(mongo_uri)
+
+db = client["teste"]
+collection = db["temperature"]
 
 @app.route('/add/temperature', methods=['POST'])
 def add_temperature(): 
@@ -9,5 +16,11 @@ def add_temperature():
     if (content_type == 'application/json'):
         json = request.json
         timestamp = datetime.now()
-        return str(timestamp)
+
+        data = {
+            "temperatura": json['temperature'],
+            "timestamp": str(timestamp)
+        }
+        result = collection.insert_one(data)
+        return jsonify({"success": True, "message": "Document inserted successfully", "id": str(result.inserted_id)})
     
